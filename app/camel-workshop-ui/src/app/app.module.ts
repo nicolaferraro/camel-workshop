d54668f7432b35dc461beed02b397ce58ad8311c
nferraro <ni.ferraro@gmail.com>
2018-05-22 00:50:43 +0200

    Added bootstrap and better ui

--- app/camel-workshop-ui/src/app/app.module.ts
+++ app/camel-workshop-ui/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './items/items.component';
 import {HttpClientModule} from "@angular/common/http";
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {AngularFontAwesomeModule} from "angular-font-awesome";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemsComponent
+    ItemsComponent,
+    AppNavbarComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule.forRoot(),
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
